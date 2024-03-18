90d2b5db1c3420b81a15ed22a9442b7f8c768858
falcotinnunculus <falcotinnunculus@github.com>
2024-03-18 11:42:47 +0100

    some markers, lines and undefined behaviors

--- ROOT_1/WidmoCs.C
+++ ROOT_1/WidmoCs.C
@@ -21,14 +21,14 @@ Bool_t WidmoCs(string name = "Cs-137.dat") {
 
 	Float_t line;
 	Int_t nbin = 0;
-	Int_t nbins = 1024;
+	const Int_t nbins = 1024;
 	Float_t counts[nbins];
 
 	while (!mydat.eof() ){
-		++nbin;
 		mydat >> line;
 		cout << line << " "; 
-		counts[nbins] = line;
+		counts[nbin] = line;
+		++nbin;
 	}
 	
 
@@ -39,9 +39,17 @@ Bool_t WidmoCs(string name = "Cs-137.dat") {
 	for(int i=0; i<nbins; i++){
 		myhist->SetBinContent(i+1,counts[i]);
 	}
-
+	
+	TCanvas *can = new TCanvas("can", "can", 500, 500);
+	can->cd();
 	myhist->Draw();
-
+ 	
+	TLine lline;
+ 	lline.DrawLine(260,0,260,1500);
+	TMarker marker;
+	marker.SetMarkerSize(2);
+	marker.SetMarkerStyle(3);
+	marker.DrawMarker(260,1240);
 
 	mydat.close();
 	return kTRUE;

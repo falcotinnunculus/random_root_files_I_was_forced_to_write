2315d7c4a05ef2df84a75aee71175fbae33b8d15
falcotinnunculus <falcotinnunculus@github.com>
2024-03-18 15:41:33 +0100

    finally works, week 3

--- ROOT_3/Fitowanie.C
+++ ROOT_3/Fitowanie.C
@@ -2,13 +2,6 @@
 #include<fstream>
 using namespace std;
 
-Double_t myfunc(Double_t *x, Double_t *par){ //TODO to na pewno da sie zrobic latwiej nie wierze
-
-	if(x[0]<175 || x[0]>350) TF1::RejectPoint();
-
-	return TMath::Exp(par[0]+par[1]*x[0])+par[2]*TMath::Exp(-0.5*TMath::Power(((x[0]-par[3])/par[4]),2));
-
-}
 
 Bool_t Fitowanie(string filename = "widmo.root", string histname = "hWidmo") {
 	cout << filename;
@@ -21,12 +14,28 @@ Bool_t Fitowanie(string filename = "widmo.root", string histname = "hWidmo") {
 	TH1F *hist = (TH1F*)file->Get("hWidmo");
 	hist->Draw();
 
-	TF1 *fun = new TF1("fun",myfunc,175,350,5);
+	TF1 *fun = new TF1("fun","expo(0)+gaus(2)",175,350);
 //	if(x[0]>175 && x[0]<350)
 //		TF1::RejectPoint();
 	fun-> SetParameters(1.23039e+01,-3.42148e-02,1.20100e+03,2.56884e+02,1.25688e+01);
 
 	TFitResultPtr results = hist->Fit(fun,"S,R");
+
+	Double_t fwhm = fun->GetParameter(4);
+	TLatex text;
+	text.DrawLatex(400,1000,to_string(fwhm).c_str());
+
+	fstream myfile;
+	myfile.open("fitowanieresults.txt",ios::out);
+	for (int i=0; i<5; i++) myfile << fun->GetParameter(i) << " ";
+	myfile << endl;
+	for (int i=0; i<5; i++) myfile << fun->GetParError(i) << " ";
+	myfile << endl;
+	myfile << results->Chi2() << endl;
+	myfile << results->Ndf() << endl;
+	myfile << results->Edm() << endl;
+	myfile << fwhm << endl;
+	myfile.close();
 
 	return kTRUE;
 

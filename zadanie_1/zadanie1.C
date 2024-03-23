13c4c282601f9196eaf7b36db2e3094e0155022a
falcotinnunculus <mateusz.m.winiarski@student.uj.edu.pl>
2024-03-24 00:23:21 +0100

    God gives his hardest battles (root) to his strongest soldiers (physics students)

--- zadanie_1/zadanie1.C
+++ zadanie_1/zadanie1.C
@@ -1,39 +1,55 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
-Bool_t zadanie1(string name="wave_0.dat"){
+Bool_t zadanie1(string name="wave_0.txt"){
 
-	TFile *file = new TFile("widmo.root","UPDATE");
+	//TFile *file = new TFile("widmo.root","UPDATE");
 
-	if(!file->IsOpen()){
-		cout << "plik nie działa" <<endl;
-		return kFALSE;
-	}
+	//if(!file->IsOpen()){
+	//	cout << "plik nie działa" <<endl;
+		//return kFALSE;
+	//}
+	//
+	cout << "dupa blada";
+	ifstream mydat;
+	mydat.open(name, ios::in );
 
-	fstream mydat;
-	mydat.open(name,ios::in | ios::binary);
-
+	cout << "dupa blada";
 	if(!mydat.is_open()){
 		cout << "dat nie działa" <<endl;
 		return kFALSE;
 	}
-	else cout << "dat działa" <<endl;
+	else {cout << "dat działa" <<endl;}
 
-
-	Float_t line;
+	cout << "dupa blada";
+	Float_t liczba;
+	cout << "dupa blada";
+	string line;
+	cout << "dupa blada";
 	Int_t nbin = 0;
+	cout << "dupa blada";
 	Int_t nbins = 1024;
+	cout << "dupa blada";
 	//Float_t counts[nbins];
-
-
-	while (!mydat.eof() ){
-		++nbin;
-		mydat >> line;
-	//	cout << line << " "; 
-		//counts[nbin] = line
-		if(nbin%10000 == 0) cout << nbin << endl;
+	Float_t temp;
+	//Int_t maxline = 55048191;
+	Int_t maxline = 55048191;
+	Float_t lines[nbins];
+	for(int i=0; i<nbins; i++) lines[i] = 0;
+	cout << "dupa";
+	while (nbin < maxline ){
+	//	if(nbin%100000==0) cout << "chuj";
+		mydat >> temp;
+		lines[nbin%1024] += temp;
+	//	if(nbin%1000000==0) cout << lines[nbin] << " ";
+	//	if(nbin%100 == 0) break;
+		nbin++;
 	}
+	for(int i=0; i<nbins; i++) cout << lines[i] << " ";
 	
+
 	cout << nbin;
 	//cout << nbins;
 	Float_t counts[nbins];
@@ -44,10 +60,10 @@ Bool_t zadanie1(string name="wave_0.dat"){
 		myhist->SetBinContent(i+1,counts[i]);
 	}
 
-	myhist->Draw();
+	//myhist->Draw();
 
 
-	mydat.close();
+	//mydat.close();
 
 	return kTRUE;
 }

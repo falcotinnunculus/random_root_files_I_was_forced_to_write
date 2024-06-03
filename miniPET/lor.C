d9c539c3b1c447276708250eb9858eb9e364a335
falcotinnunculus <mateusz.m.winiarski@student.uj.edu.pl>
2024-06-03 14:15:24 +0200

    makes a nice graph, again

--- miniPET/lor.C
+++ miniPET/lor.C
@@ -15,122 +15,121 @@ Float_t gety(Float_t X, Int_t det){
 
 void lor(void){ 
 
-Int_t Qthr=400; // ustawienie progu dyskryminacji impulsow
-Int_t QX1[ndet], QX2[ndet], QZ1[ndet], QZ2[ndet], QX[ndet], QZ[ndet]; 
-Float_t X[ndet], Z[ndet];
-Float_t x[2], y[2];
-Float_t xx, yy;
-Float_t A, B, s, phi;
+    Int_t Qthr=400; // ustawienie progu dyskryminacji impulsow
+    Int_t QX1[ndet], QX2[ndet], QZ1[ndet], QZ2[ndet], QX[ndet], QZ[ndet]; 
+    Float_t X[ndet], Z[ndet];
+    Float_t x[2], y[2];
+    Float_t xx, yy;
+    Float_t A, B, s, phi;
 
-// otworz plik z danymi w formacie root
-TFile *hfile = TFile::Open("QDC.root"); 
-TTree *tree = (TTree*)hfile->Get("tree");
-tree->SetBranchAddress("QX1",&QX1);
-tree->SetBranchAddress("QX2",&QX2);
-tree->SetBranchAddress("QZ1",&QZ1);
-tree->SetBranchAddress("QZ2",&QZ2);
+    // otworz plik z danymi w formacie root
+    TFile *hfile = TFile::Open("QDC.root"); 
+    TTree *tree = (TTree*)hfile->Get("tree");
+    tree->SetBranchAddress("QX1",&QX1);
+    tree->SetBranchAddress("QX2",&QX2);
+    tree->SetBranchAddress("QZ1",&QZ1);
+    tree->SetBranchAddress("QZ2",&QZ2);
 
-// utworzenie histogramow
-TH1F *hQX[ndet], *hQZ[ndet];
-TH2F *hQXZ[ndet];
+    // utworzenie histogramow
+    TH1F *hQX[ndet], *hQZ[ndet];
+    TH2F *hQXZ[ndet];
 
-// definicja histogramow
-for(Int_t ii=0;ii<ndet ;ii++)
-{
-	TString name1;
-	name1="X for detector";   name1+=ii+1;
-	hQX[ii]=new TH1F(name1,name1,100, -xscale,xscale);
-	name1="Z for detector";   name1+=ii+1;
-	hQZ[ii]=new TH1F(name1,name1,100, -xscale,xscale);
+    // definicja histogramow
+    for(Int_t ii=0;ii<ndet ;ii++)
+    {
+        TString name1;
+        name1="X for detector";   name1+=ii+1;
+        hQX[ii]=new TH1F(name1,name1,100, -xscale,xscale);
+        name1="Z for detector";   name1+=ii+1;
+        hQZ[ii]=new TH1F(name1,name1,100, -xscale,xscale);
 
-	name1="X vs Z for detector";   name1+=ii+1;
-	hQXZ[ii]=new TH2F(name1,name1,100, -xscale,xscale, 100, -xscale,xscale);
-}
+        name1="X vs Z for detector";   name1+=ii+1;
+        hQXZ[ii]=new TH2F(name1,name1,100, -xscale,xscale, 100, -xscale,xscale);
+    }
 
 
-TString name2;
-name2="x";
-TH1F *hx=new TH1F(name2,name2,100, -R*1.2,R*1.2);
-name2="y";
-TH1F *hy=new TH1F(name2,name2,100, -R*1.2,R*1.2);
-name2="x vs y";
-TH2F *hxy=new TH2F(name2,name2,200,-100,100,200,-100,100);
-name2="s vs phi";
-TH2F *hsphi=new TH2F(name2,name2,200,-100,100,200,-2,2);
+    TString name2;
+    name2="x";
+    TH1F *hx=new TH1F(name2,name2,100, -R*1.2,R*1.2);
+    name2="y";
+    TH1F *hy=new TH1F(name2,name2,100, -R*1.2,R*1.2);
+    name2="x vs y";
+    TH2F *hxy=new TH2F(name2,name2,200,-100,100,200,-100,100);
+    name2="s vs phi";
+    TH2F *hsphi=new TH2F(name2,name2,200,-100,100,200,-2,2);
 
-// petla po zdarzeniach
-Long64_t nentries = tree->GetEntries(); // wczytuje liczbe zdarzen
-for (Long64_t i=0; i<nentries;i++) 
-{
-	tree->GetEntry(i); // wczytuje zdarzenie
-	
-	Int_t mult =0; // liczba detektorow z ktorych sygnaly przekroczyly odpowiednie progi
-	for(Int_t ii=0;ii<ndet ;ii++) // petla po detektorach
-	{		
-// sprawdz czy sygnaly przekraczaja prog
-		if((QX1[ii]>Qthr) && (QX2[ii]>Qthr) && (QZ1[ii]>Qthr) && (QZ2[ii]>Qthr)) 
-		{  
-// tutaj można wstawić wypelnianie roznych histogramow			
+    // petla po zdarzeniach
+    Long64_t nentries = tree->GetEntries(); // wczytuje liczbe zdarzen
+    for (Long64_t i=0; i<nentries;i++) 
+    {
+        tree->GetEntry(i); // wczytuje zdarzenie
 
-		//}
-        //else{
-            //continue;
-            //break;
-        //}
-        X[ii] = ((Float_t)QX2[ii]-(Float_t)QX1[ii])/((Float_t)QX1[ii]+(Float_t)QX2[ii])*xscale;
-		hQX[ii]->Fill(X[ii]);
-        Z[ii] = ((Float_t)QZ2[ii]-(Float_t)QZ1[ii])/((Float_t)QZ1[ii]+(Float_t)QZ2[ii])*xscale;
-		hQZ[ii]->Fill(Z[ii]);
-		hQXZ[ii]->Fill(X[ii],Z[ii]);
-       
-        x[mult] = getx(X[ii],ii);
-        y[mult] = gety(X[ii],ii);
+        Int_t mult =0; // liczba detektorow z ktorych sygnaly przekroczyly odpowiednie progi
+        for(Int_t ii=0;ii<ndet ;ii++) // petla po detektorach
+        {		
+            // sprawdz czy sygnaly przekraczaja prog
+            if((QX1[ii]>Qthr) && (QX2[ii]>Qthr) && (QZ1[ii]>Qthr) && (QZ2[ii]>Qthr)) 
+            {  
+                // tutaj można wstawić wypelnianie roznych histogramow			
 
-		mult++;
-        if(mult==2){
-            hx->Fill(x[0]);
-            hy->Fill(y[0]);
-            hxy->Fill(x[0],y[0]);
-            hx->Fill(x[1]);
-            hy->Fill(y[1]);
-            hxy->Fill(x[1],y[1]);
+                //}
+                //else{
+                //continue;
+                //break;
+                //}
+            X[ii] = ((Float_t)QX2[ii]-(Float_t)QX1[ii])/((Float_t)QX1[ii]+(Float_t)QX2[ii])*xscale;
+            hQX[ii]->Fill(X[ii]);
+            Z[ii] = ((Float_t)QZ2[ii]-(Float_t)QZ1[ii])/((Float_t)QZ1[ii]+(Float_t)QZ2[ii])*xscale;
+            hQZ[ii]->Fill(Z[ii]);
+            hQXZ[ii]->Fill(X[ii],Z[ii]);
 
-//for(int iii=0; iii<400; iii++)
-//{
-//    xx=x[1]+(float)iii/400.0*(x[2]-x[1]); yy=y[1]+(float)iii/400.0*(y[2]-y[1]);
-//    hxy->Fill(xx,yy);
-//}
+            x[mult] = getx(X[ii],ii);
+            y[mult] = gety(X[ii],ii);
 
-        A = (y[1]-y[0])/(x[1]-x[0]);
-        B = (y[0]*x[1]-y[1]*x[0])/(x[1]-x[0]);
-        s = (B)/(TMath::Sqrt(A*A+1));
-        phi = TMath::ATan(-1/A);
+            mult++;
+            if(mult==2){
+                hx->Fill(x[0]);
+                hy->Fill(y[0]);
+                hxy->Fill(x[0],y[0]);
+                hx->Fill(x[1]);
+                hy->Fill(y[1]);
+                hxy->Fill(x[1],y[1]);
 
-        hsphi->Fill(s,phi);
+                //for(int iii=0; iii<400; iii++)
+                //{
+                //    xx=x[1]+(float)iii/400.0*(x[2]-x[1]); yy=y[1]+(float)iii/400.0*(y[2]-y[1]);
+                //    hxy->Fill(xx,yy);
+                //}
 
-            break;
-        }
-        
-        }
-	}
+                A = (y[1]-y[0])/(x[1]-x[0]);
+                B = (y[0]*x[1]-y[1]*x[0])/(x[1]-x[0]);
+                s = (B)/(TMath::Sqrt(A*A+1));
+                phi = TMath::ATan(-1/A);
 
-} // koniec petli po zdarzeniach
+                hsphi->Fill(s,phi);
+
+                break;
+            } //koniec ifa po mult
+
+        } //koniec pętli po detektorach
+    } //koniec pętli po zdarzeniach
 
 
 
 
-TCanvas* d4 = new TCanvas("d4", "Histograms", 200,160, 1200, 1200); //utworz kanwe
-d4->Divide(2,2);	
-d4->cd(1);
-hx->Draw();
-d4->cd(2);
-hy->Draw();
-d4->cd(3);
-hxy->Draw();
-d4->cd(4);
-hsphi->Draw("colz");
-d4->Update();
-//d4->WaitPrimitive();
 
-return;
+    TCanvas* d4 = new TCanvas("d4", "Histograms", 200,160, 1200, 1200); //utworz kanwe
+    d4->Divide(2,2);	
+    d4->cd(1);
+    hx->Draw();
+    d4->cd(2);
+    hy->Draw();
+    d4->cd(3);
+    hxy->Draw();
+    d4->cd(4);
+    hsphi->Draw("colz");
+    d4->Update();
+    //d4->WaitPrimitive();
+
+    return;
 }
